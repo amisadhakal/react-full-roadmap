e0b764c237729edc4474fdfde2a00332ce72d90f
amisadhakal <amisadhakal3@gmail.com>
2026-07-25 23:26:57 +0545

    Update app.py

--- react-fullstack-application/backend/app.py
+++ react-fullstack-application/backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -18,6 +18,19 @@ tasks = [
 @app.route("/tasks")
 def get_tasks():
     return jsonify(tasks)
+
+@app.route("/tasks", methods=["POST"])
+def add_task():
+    data = request.get_json()
+
+    new_task = {
+        "id": len(tasks) + 1,
+        "title": data["title"]
+    }
+
+    tasks.append(new_task)
+
+    return jsonify(new_task), 201
 
 if __name__ == "__main__":
     app.run(debug=True)
